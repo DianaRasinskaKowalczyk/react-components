08b88b272331c7d980e348cd3e0b89c463184f3b
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2023-04-24 14:35:55 +0200

    Add submitHandler

--- src/components/TasksManager.js
+++ src/components/TasksManager.js
@@ -1,18 +1,85 @@
-import React from 'react';
+import React from "react";
 
 class TasksManager extends React.Component {
-    state = {
-        tasks: [],
-    }
+	state = {
+		tasks: [],
+		task: {
+			name: "",
+			id: "",
+			time: "00:00:00",
+			isRunning: "false",
+			isDone: "false",
+			isRemoved: "false",
+		},
+	};
 
-    onClick = () => {
-        const { tasks } = this.state;
-        console.log( tasks)
-    }
+	inputHandler = e => {
+		const { value } = e.target;
+		this.setState({
+			task: {
+				name: value,
+			},
+		});
+	};
 
-    render() {
-        return <h1 onClick={ this.onClick }>TasksManager</h1>
-    }
+	onClick = () => {
+		const { tasks } = this.state;
+		console.log(tasks);
+	};
+
+	render() {
+		const { task } = this.state;
+
+		return (
+			<>
+				<h1 onClick={this.onClick}>Tasks Manager</h1>
+				<section>
+					<form onSubmit={this.submitHandler}>
+						<label>
+							Wpisz nazwę zadania:
+							<input
+								name='task'
+								value={task.name}
+								onChange={this.inputHandler}></input>
+							<input type='submit' />
+						</label>
+					</form>
+					<header>Zadanie 1, 00:00:00</header>
+					<footer>
+						<button>start/stop</button>
+						<button>zakończone</button>
+						<button disabled='true'>usuń</button>
+					</footer>
+				</section>
+			</>
+		);
+	}
+
+	submitHandler = e => {
+		e.preventDefault();
+
+		const { task, tasks } = this.state;
+		const options = {
+			method: "POST",
+			body: JSON.stringify(task),
+			headers: { "Content-Type": "application/json" },
+		};
+
+		const promise = fetch("http://localhost:3005/data", options);
+		return promise
+			.then(resp => {
+				if (resp.ok) {
+					return resp.json();
+				}
+				return Promise.reject(resp);
+			})
+			.catch(err => console.error(err))
+			.then(data => {
+				this.setState({
+					tasks: [...tasks, data.id],
+				});
+			});
+	};
 }
 
 export default TasksManager;
