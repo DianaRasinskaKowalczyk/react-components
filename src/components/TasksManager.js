dfd00abf41009d7055fe9928aedfedc052db7c57
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2023-04-25 18:51:42 +0200

    Add rendering

--- src/components/TasksManager.js
+++ src/components/TasksManager.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { postFetch } from "./handleApi";
 
 class TasksManager extends React.Component {
 	state = {
@@ -6,31 +7,38 @@ class TasksManager extends React.Component {
 		task: {
 			name: "",
 			id: "",
-			time: "00:00:00",
+			time: "0",
 			isRunning: "false",
 			isDone: "false",
 			isRemoved: "false",
 		},
 	};
 
-	inputHandler = e => {
-		const { value } = e.target;
-		this.setState({
-			task: { name: value },
-		});
-	};
-
-	onClick = () => {
+	renderTask() {
 		const { tasks } = this.state;
-		console.log(tasks);
-	};
+
+		return tasks.map(task => {
+			return (
+				<li>
+					<header>
+						<h2>{task.name}</h2>
+						<p>{this.createTimer(task.time)}</p>
+					</header>
+					<footer>
+						<button>{task.isRunning ? "start" : "stop"}</button>
+						<button>zakończone</button>
+						<button disabled='true'>usuń</button>
+					</footer>
+				</li>
+			);
+		});
+	}
 
 	render() {
 		const { task } = this.state;
-
 		return (
 			<>
-				<h1 onClick={this.onClick}>Tasks Manager</h1>
+				<h1 onClick={this.clicker}>Tasks Manager</h1>
 				<section>
 					<form onSubmit={this.submitHandler}>
 						<label>
@@ -42,42 +50,56 @@ class TasksManager extends React.Component {
 							<input type='submit' />
 						</label>
 					</form>
-					<header>Zadanie 1, 00:00:00</header>
-					<footer>
-						<button>start/stop</button>
-						<button>zakończone</button>
-						<button disabled='true'>usuń</button>
-					</footer>
+					<ul>{this.renderTask()}</ul>
 				</section>
 			</>
 		);
 	}
 
+	createTimer(time) {
+		let hrs = Math.floor(time / 3600);
+		let mins = Math.floor((time - hrs * 3600) / 60);
+		let secs = time % 60;
+
+		if (secs < 10) {
+			secs = "0" + secs;
+		}
+		if (mins < 10) {
+			mins = "0" + mins;
+		}
+		if (hrs < 10) {
+			hrs = "0" + hrs;
+		}
+		return `${hrs}:${mins}:${secs}`;
+	}
+
+	inputHandler = e => {
+		const { task } = this.state;
+		const { value } = e.target;
+		this.setState({
+			task: { ...task, name: value },
+		});
+	};
+
+	clicker = () => {
+		console.log(this.state.tasks);
+	};
+
 	submitHandler = e => {
 		e.preventDefault();
+		const { task } = this.state;
 
-		const { task, tasks } = this.state;
-		const options = {
-			method: "POST",
-			body: JSON.stringify(task),
-			headers: { "Content-Type": "application/json" },
-		};
-
-		const promise = fetch("http://localhost:3005/data", options);
-		return promise
-			.then(resp => {
-				if (resp.ok) {
-					return resp.json();
-				}
-				return Promise.reject(resp);
-			})
-			.catch(err => console.error(err))
-			.then(data => {
-				this.setState({
-					tasks: [...tasks, data.id],
-				});
-			});
+		postFetch(task);
+		this.addTask(task);
 	};
+
+	addTask(newTask) {
+		const { tasks, task } = this.state;
+		this.setState({
+			tasks: [...tasks, newTask],
+			task: { ...task, name: "" },
+		});
+	}
 }
 
 export default TasksManager;
---
+++ src/components/handleApi.js
@@ -0,0 +1,24 @@
+const url = "http://localhost:3005/data/";
+
+export function postFetch(task) {
+	const options = {
+		method: "POST",
+		body: JSON.stringify(task),
+		headers: { "Content-Type": "application/json" },
+	};
+
+	return handleFetch(options);
+}
+
+export function handleFetch(options, additionalPath = "") {
+	const path = url + additionalPath;
+	const promise = fetch(path, options);
+	return promise
+		.then(resp => {
+			if (resp.ok) {
+				return resp.json();
+			}
+			return Promise.reject(resp);
+		})
+		.catch(err => console.error(err));
+}
